a7ce1ae619f3e4fe10dd059aa9b1828058401926
NikitaKotovAbs <isip_n.i.charny@mpt.ru>
2022-12-21 19:19:33 +0300

    Добавьте файлы проекта.

---
+++ Laba10/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba10
+{
+
+    internal class Switch
+    {
+        public static void Arrow()
+        {
+            int position = 1;
+            while (true)
+            {
+                Console.WriteLine("\t\tДобро пожаловать в магазин ВСЕ ДЛЯ ВАС");
+                Console.WriteLine("   Логин: ");
+                Console.WriteLine("   Пороль: ");
+                Console.WriteLine("Авторизироваться");
+
+                Console.SetCursorPosition(0, position);
+                Console.WriteLine("->");
+                ConsoleKeyInfo key = Console.ReadKey();
+                Console.Clear();
+
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    position = 2;
+                }
+                if (key.Key == ConsoleKey.DownArrow)
+                {
+                    position = position + 1;
+                }
+                if (key.Key == ConsoleKey.UpArrow)
+                {
+                    position = position - 1;
+                }
+                if (key.Key == ConsoleKey.DownArrow)
+                {
+                    position = 2;
+                }
+                if (key.Key == ConsoleKey.Enter)
+                {
+                    if (position == 1)
+                    {
+                        Console.WriteLine("\t\tДобро пожаловать в магазин ВСЕ ДЛЯ ВАС");
+                          Console.WriteLine("   Логин: "); Console.SetCursorPosition(10, 1); Console.ReadLine();
+                        
+                        /*Console.WriteLine(  ); Console.ReadLine(); Console.SetCursorPosition(10, 1);*/
+                    }
+                    
+                }
+                Console.WriteLine("   Пороль: ");
+                Console.WriteLine("Авторизироваться");
+            }
+        }
+    }
+}
+
---
+++ Laba10/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Laba10
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Switch.Arrow();
+                
+        }
+       
+    }
+}
+
+/*Console.WriteLine("Логин: "); Console.SetCursorPosition(10, 1);
+Console.ReadLine();*/
